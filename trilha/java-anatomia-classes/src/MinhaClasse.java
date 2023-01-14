34554b4f0062043aec5038f7e1ff57eaeec8b831
igor <igorkazuhico@hotmail.com>
2023-01-14 13:05:38 -0300

    anatomia das classes - 3

--- trilha/java-anatomia-classes/src/MinhaClasse.java
+++ trilha/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,13 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("ola");
+    String primeiroNome ="Igor";
+    String segundoNome = "Kazuhiko";
+    String nomeCompleto =nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+    
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Rsultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
